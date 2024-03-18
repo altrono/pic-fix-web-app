c0c9267534340b268054b9422d5f199448376a2f
altrono <hbotema16@gmail.com>
2024-03-18 21:13:55 +0200

    finish transform image

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -23,6 +23,7 @@ import { useState, useTransition } from "react";
 import { AspectRatioKey, debounce, deepMergeObjects } from "@/lib/utils";
 import { updateCredits } from "@/lib/actions/user.actions";
 import MediaUploader from "./MediaUploader";
+import TransformedImage from "./TransformedImage";
 
 export const formSchema = z.object({
     title: z.string().min(3).max(50),
@@ -201,9 +202,18 @@ const TransformationForm = ({ action, data = null, userId, type, creditBalance, 
                             publicId={field.value}
                             image={image}
                             type={type}
-                            
+
                         />
                     )}
+                />
+
+                <TransformedImage 
+                    image={image}
+                    type={type}
+                    title={form.getValues().title}
+                    isTransforming={isTransforming}
+                    setIsTransforming={setIsTransforming}
+                    transformationConfig={transformationConfig}
                 />
             </div>
 
