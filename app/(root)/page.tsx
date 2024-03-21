e2cd0c1496d41d0557a88d0a24d90d16033a86e9
altrono <hbotema16@gmail.com>
2024-03-21 20:23:06 +0200

    implement Image details

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,19 @@
 import { Collection } from '@/components/shared/Collection';
 import { navLinks } from '@/constants';
+import { getAllImages } from '@/lib/actions/image.actions';
 import { UserButton } from '@clerk/nextjs';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react'
 
 
-const Home = ({searchParams} : SearchParamProps) => {
+const Home = async ({searchParams} : SearchParamProps) => {
   const page = Number(searchParams?.page) || 1;
   const searchQuery = (searchParams?.query as string) || "";
+
+  const images = await getAllImages({
+    page, searchQuery
+  });
 
   return (
     <>
@@ -38,7 +43,12 @@ const Home = ({searchParams} : SearchParamProps) => {
       </section>
 
       <section className="sm:mt-12">
-        <Collection images={[]} />
+        <Collection 
+          hasSearch={true}
+          images={images?.data} 
+          totalPages={images?.totalPage}
+          page={page} 
+          />
       </section>
     </>
   )
